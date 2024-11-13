e3f7854b64c8047c777c8a977b54fe78592dd715
ssyedarif <ssyedarif98@gmail.com>
2024-11-13 20:29:10 +0800

    darkmode

--- client/src/components/navbar.tsx
+++ client/src/components/navbar.tsx
@@ -19,16 +19,27 @@ import {
   NavigationMenuTrigger,
   NavigationMenuViewport,
 } from "@/components/ui/navigation-menu";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 import Link from "next/link";
 import { navigationMenuTriggerStyle } from "@/components/ui/navigation-menu";
+import { useTheme } from "next-themes";
+import { Moon, Sun } from "lucide-react";
 
 export default function Navbar() {
+  const { setTheme } = useTheme();
+
   return (
-    <div className="m-5 fixed top-0 left-0 right-0">
+    <div className="m-5 fixed top-0 left-0 right-0 ">
       <Card>
-        <div className="p-3 flex justify-between">
+        <div className="p-3 flex flex-row justify-between container mx-auto">
           <div className="my-auto">LeaveANote </div>
-          <div>
+          <div className="flex flex-row gap-5">
             <NavigationMenu>
               <NavigationMenuList>
                 <NavigationMenuItem>
@@ -60,6 +71,26 @@ export default function Navbar() {
                 </NavigationMenuItem>
               </NavigationMenuList>
             </NavigationMenu>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button variant="outline" size="icon">
+                  <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+                  <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+                  <span className="sr-only">Toggle theme</span>
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent align="end">
+                <DropdownMenuItem onClick={() => setTheme("light")}>
+                  Light
+                </DropdownMenuItem>
+                <DropdownMenuItem onClick={() => setTheme("dark")}>
+                  Dark
+                </DropdownMenuItem>
+                <DropdownMenuItem onClick={() => setTheme("system")}>
+                  System
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
           </div>
         </div>
       </Card>
