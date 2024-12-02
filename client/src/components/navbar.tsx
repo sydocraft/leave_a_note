e0cc17bedfb1876f11099cc0f03f3ca43d714c54
ssyedarif <ssyedarif98@gmail.com>
2024-12-03 06:04:33 +0800

    layout responsive navbar

--- client/src/components/navbar.tsx
+++ client/src/components/navbar.tsx
@@ -10,6 +10,15 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+
+import {
   NavigationMenu,
   NavigationMenuContent,
   NavigationMenuIndicator,
@@ -29,7 +38,7 @@ import {
 import Link from "next/link";
 import { navigationMenuTriggerStyle } from "@/components/ui/navigation-menu";
 import { useTheme } from "next-themes";
-import { Moon, Sun } from "lucide-react";
+import { Moon, Sun, Menu } from "lucide-react";
 import { Sour_Gummy } from "next/font/google";
 
 const SG = Sour_Gummy({ subsets: ["latin"] });
@@ -45,7 +54,75 @@ export default function Navbar() {
             <Link href="/">LeaveANote</Link>
           </div>
           <div className="flex flex-row gap-5">
-            <NavigationMenu>
+            <Sheet>
+              <SheetTrigger className="sm:hidden">
+                <Button variant="outline" size="icon">
+                  <Menu />
+                </Button>
+              </SheetTrigger>
+              <SheetContent>
+                <SheetHeader>
+                  <SheetTitle className={`${SG.className} my-auto text-xl`}>
+                    LeaveANote
+                  </SheetTitle>
+                </SheetHeader>
+
+                <NavigationMenu className="mx-auto mt-10">
+                  <NavigationMenuList className="gap-5 flex-col">
+                    <NavigationMenuItem>
+                      <Link href="/createnote" legacyBehavior passHref>
+                        <NavigationMenuLink
+                          className={navigationMenuTriggerStyle()}
+                        >
+                          Get Started
+                        </NavigationMenuLink>
+                      </Link>
+                    </NavigationMenuItem>
+                    <NavigationMenuItem>
+                      <Link href="/browsenote" legacyBehavior passHref>
+                        <NavigationMenuLink
+                          className={navigationMenuTriggerStyle()}
+                        >
+                          Browse
+                        </NavigationMenuLink>
+                      </Link>
+                    </NavigationMenuItem>
+                    <NavigationMenuItem>
+                      <Link href="/supportus" legacyBehavior passHref>
+                        <NavigationMenuLink
+                          className={navigationMenuTriggerStyle()}
+                        >
+                          Support
+                        </NavigationMenuLink>
+                      </Link>
+                    </NavigationMenuItem>
+                  </NavigationMenuList>
+                </NavigationMenu>
+                <div className="flex justify-center mt-10">
+                  <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                      <Button variant="outline" size="icon">
+                        <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+                        <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+                        <span className="sr-only">Toggle theme</span>
+                      </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent align="end">
+                      <DropdownMenuItem onClick={() => setTheme("light")}>
+                        Light
+                      </DropdownMenuItem>
+                      <DropdownMenuItem onClick={() => setTheme("dark")}>
+                        Dark
+                      </DropdownMenuItem>
+                      <DropdownMenuItem onClick={() => setTheme("system")}>
+                        System
+                      </DropdownMenuItem>
+                    </DropdownMenuContent>
+                  </DropdownMenu>
+                </div>
+              </SheetContent>
+            </Sheet>
+            <NavigationMenu className="hidden sm:block">
               <NavigationMenuList className="gap-3">
                 <NavigationMenuItem>
                   <Link href="/createnote" legacyBehavior passHref>
@@ -78,7 +155,11 @@ export default function Navbar() {
             </NavigationMenu>
             <DropdownMenu>
               <DropdownMenuTrigger asChild>
-                <Button variant="outline" size="icon">
+                <Button
+                  variant="outline"
+                  size="icon"
+                  className="hidden sm:flex"
+                >
                   <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
                   <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
                   <span className="sr-only">Toggle theme</span>
