f89cd7d92ecb7867ed98de9d97fd2a5ddfff55ec
ssyedarif <ssyedarif98@gmail.com>
2024-12-05 11:02:04 +0800

    cors meh

--- server/index.js
+++ server/index.js
@@ -15,6 +15,8 @@ app.use(
   cors({ origin: "https://leaveanote-alpha.vercel.app", credentials: true })
 );
 
+app.options("*", cors()); // Allow preflight requests for all routes
+
 // use the body-parser middleware to parse JSON and URL-encoded data
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
